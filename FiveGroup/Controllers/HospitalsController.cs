ed1f10c5a6e9b88bd997d18e59f84b017495742d
Jason Wu <bluereindeer@hotmail.com>
2019-11-06 21:57:26 +0800

    添加分頁功能

--- FiveGroup/Controllers/HospitalsController.cs
+++ FiveGroup/Controllers/HospitalsController.cs
@@ -8,25 +8,60 @@ using System.Web;
 using System.Web.Mvc;
 using FiveGroup.Models;
 using FiveGroup.ViewModel;
+using PagedList;
 
 namespace FiveGroup.Controllers
 {
     public class HospitalsController : Controller
     {
         private Project2Entities db = new Project2Entities();
-
+        private int pageSize = 3;
         // GET: Hospitals
-        public ActionResult Index(int cityname = 1)
+        public ActionResult Index(int? cityId, int? districtId, int page = 1)
         {
-            HospitalLocation HL = new HospitalLocation()
-            {
-                Hospitals = db.hospital.Where(m => m.c_id == cityname).ToList(),
-                Cities = db.city.ToList(),
-                Districts = db.district.ToList()
-            };
+            HospitalLocation HL;
+            int currentPage = page < 1 ? 1 : page;
 
-            ViewBag.cityname = db.city.Where(m => m.c_id == cityname).FirstOrDefault().city_name;
-            ViewBag.citynameID = cityname;
+            if (cityId == null & districtId == null)
+            {
+                HL = new HospitalLocation()
+                {
+                    //Hospitals = db.hospital.ToList(),
+                    Cities = db.city.ToList(),
+                    Districts = db.district.ToList(),
+                    HospitalPages = db.hospital.ToList().ToPagedList(currentPage, pageSize)
+                };
+
+                ViewBag.cityname = "全部縣市";
+                ViewBag.cityID = "null";
+                //ViewBag.d_id = new SelectList(db.district, "d_id", "district_name");
+            }
+            else if (cityId != null & districtId == null)
+            {
+                HL = new HospitalLocation()
+                {
+                    HospitalPages = db.hospital.Where(m => m.c_id == cityId).ToList().ToPagedList(currentPage, pageSize),
+                    Cities = db.city.ToList(),
+                    Districts = db.district.Where(m => m.c_id == cityId).ToList()
+                };
+
+                ViewBag.cityname = db.city.Where(m => m.c_id == cityId).FirstOrDefault().city_name;
+                ViewBag.cityID = cityId;
+                //ViewBag.d_id = new SelectList(db.district.Where(m => m.c_id == cityId), "d_id", "district_name");
+            }
+            else
+            {
+                HL = new HospitalLocation()
+                {
+                    HospitalPages = db.hospital.Where(m => m.c_id == cityId & m.d_id == districtId).ToList().ToPagedList(currentPage, pageSize),
+                    Cities = db.city.ToList(),
+                    Districts = db.district.Where(m => m.c_id == cityId).ToList()
+                };
+
+                ViewBag.cityname = db.city.Where(m => m.c_id == cityId).FirstOrDefault().city_name;
+                ViewBag.cityID = cityId;
+                //ViewBag.d_id = new SelectList(db.district.Where(m => m.c_id == cityId), "d_id", "district_name");
+            }
 
             return View(HL);
 
@@ -34,23 +69,33 @@ namespace FiveGroup.Controllers
 
 
         // GET: Hospitals/Create
-        public ActionResult Create(int? cityname)
+        public ActionResult Create(int? cityId)
         {
-            if (cityname == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            /*取資料筆數並呼叫New_Hos_id獲取新id，再透過ViewBag傳至前端*/
             var c = db.hospital.Count();
             string id;
             id = New_Hos_id(c);
             ViewBag.hos_id = id;
+
+            if (cityId == null)
+            {
+
+                ViewBag.cityID = "null";
+                ViewBag.cityname = new SelectList(db.city, "c_id", "city_name");
+                ViewBag.d_id = new SelectList(db.district, "d_id", "district_name");
+                return View();
+                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            /*取資料筆數並呼叫New_Hos_id獲取新id，再透過ViewBag傳至前端*/
+            //var c = db.hospital.Count();
+            //string id;
+            //id = New_Hos_id(c);
+            //ViewBag.hos_id = id;
             /************************************************************/
 
-            ViewBag.citynameID = cityname;
-            ViewBag.cityname = new SelectList(db.city.Where(m => m.c_id == cityname), "c_id", "city_name");
-            ViewBag.d_id = new SelectList(db.district.Where(m => m.c_id == cityname), "d_id", "district_name");
+            ViewBag.cityID = cityId;
+            ViewBag.cityname = new SelectList(db.city.Where(m => m.c_id == cityId), "c_id", "city_name");
+            ViewBag.d_id = new SelectList(db.district.Where(m => m.c_id == cityId), "d_id", "district_name");
             return View();
         }
 
@@ -58,28 +103,37 @@ namespace FiveGroup.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "hos_id,hos_name,hos_eng_name,hos_address,hos_phone,hos_display,c_id,d_id")] hospital hospital, int cityname)
+        public ActionResult Create([Bind(Include = "hos_id,hos_name,hos_eng_name,hos_address,hos_phone,hos_display,c_id,d_id")] hospital hospital, int? cityId)
         {
+            //var c = db.hospital.Count();
+            //string id;
+            //id = New_Hos_id(c);
+
             if (ModelState.IsValid)
             {
+                //hospital = new hospital() { hos_id = id };
                 db.hospital.Add(hospital);
                 db.SaveChanges();
-                return RedirectToAction("Index", new { cityname = cityname });
+                if (cityId == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return RedirectToAction("Index", new { cityId = cityId });
             }
 
-            var c = db.hospital.Count();
-            string id;
-            id = New_Hos_id(c);
-            ViewBag.hos_id = id;
+            //var c = db.hospital.Count();
+            //string id;
+            //id = New_Hos_id(c);
+            //ViewBag.hos_id = id;
 
-            ViewBag.cityname = new SelectList(db.city.Where(m => m.c_id == cityname), "c_id", "city_name");
-            ViewBag.d_id = new SelectList(db.district.Where(m => m.c_id == cityname), "d_id", "district_name");
+            ViewBag.cityname = new SelectList(db.city.Where(m => m.c_id == cityId), "c_id", "city_name");
+            ViewBag.d_id = new SelectList(db.district.Where(m => m.c_id == cityId), "d_id", "district_name");
 
             return View(hospital);
         }
 
         // GET: Hospitals/Edit/5
-        public ActionResult Edit(string id, int cityname)
+        public ActionResult Edit(string id, int cityId)
         {
             if (id == null)
             {
@@ -91,7 +145,7 @@ namespace FiveGroup.Controllers
                 return HttpNotFound();
             }
 
-            ViewBag.citynameID = cityname;
+            ViewBag.cityID = cityId;
             ViewBag.c_id = new SelectList(db.city, "c_id", "city_name", hospital.c_id);
             ViewBag.d_id = new SelectList(db.district, "d_id", "district_name", hospital.d_id);
             return View(hospital);
@@ -100,13 +154,13 @@ namespace FiveGroup.Controllers
         // POST: Hospitals/Edit/5        
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "hos_id,hos_name,hos_eng_name,hos_address,hos_phone,hos_display,c_id,d_id")] hospital hospital, int cityname)
+        public ActionResult Edit([Bind(Include = "hos_id,hos_name,hos_eng_name,hos_address,hos_phone,hos_display,c_id,d_id")] hospital hospital, int cityId)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(hospital).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index", new { cityname = cityname });
+                return RedirectToAction("Index", new { cityId = cityId });
             }
             ViewBag.c_id = new SelectList(db.city, "c_id", "city_name", hospital.c_id);
             ViewBag.d_id = new SelectList(db.district, "d_id", "district_name", hospital.d_id);
--- FiveGroup/ViewModel/HospitalLocation.cs
+++ FiveGroup/ViewModel/HospitalLocation.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PagedList;
+using PagedList.Mvc;
 
 namespace FiveGroup.ViewModel
 {
@@ -11,6 +13,7 @@ namespace FiveGroup.ViewModel
         public List<hospital> Hospitals { get; set; }
         public List<city> Cities { get; set; }
         public List<district> Districts { get; set; }
+        public IPagedList<hospital> HospitalPages { get; set; }
 
     }
 }
